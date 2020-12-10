21e21cb2991d69485192f397b694f768773fd57d
yogiex <priyagung.yoggi@gmail.com>
2020-12-10 19:33:10 +0700

    server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 
 
+
 app.use('/', express.static(__dirname + '/'));
 app.get('/*',(req,res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
